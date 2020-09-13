c280592d06cb116a4cf2ca829918501d2904442e
LAzar5544 <71109530+LAzar5544@users.noreply.github.com>
2020-09-13 12:56:24 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+
+ if(isTouching(movingRect,fixedRect)){
+   movingRect.shapeColor="blue";
+   fixedRect.shapeColor="blue";
+ }
+ else{
+  movingRect.shapeColor="green";
+  fixedRect.shapeColor="green";
+ }
+
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+  && object2.y - object1.y < object2.height/2 + object1.height/2){
+    return true;
+  }
+
+    else{
+      return false;
+    }
+ 
+}
+
